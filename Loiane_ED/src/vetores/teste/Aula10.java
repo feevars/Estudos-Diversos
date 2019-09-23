d0ccb3dcb14067082d20a5bac1c1064c87df4e0a
Fernanda Reis <fpreis98@gmail.com>
2019-09-23 17:00:38 -0300

    aula 10

--- Loiane_ED/src/vetores/teste/Aula10.java
+++ Loiane_ED/src/vetores/teste/Aula10.java
@@ -9,21 +9,25 @@ public class Aula10 {
 		
 		VetorObjetos vetor = new VetorObjetos(3);
 		
-		vetor.adiciona(3);
-		vetor.adiciona(4);
-		vetor.adiciona(5);
+		Contato c1 = new Contato("Contato 1", "9999-9999", "contato1@gmail.com");
+		Contato c2 = new Contato("Contato 2", "9999-7959", "contato2@gmail.com");
+		Contato c3 = new Contato("Contato 3", "9996-6649", "contato3@gmail.com");
+		Contato c4 = new Contato("Contato 4", "9666-2649", "contato4@gmail.com");
 		
+		vetor.adiciona(c1); //int
+		vetor.adiciona(c2); //String
+		vetor.adiciona(c3); //double
 		
-		System.out.println(vetor); //exibe vetor
+		System.out.println("Tamanho = " + vetor.tamanho()); 
+		System.out.println(vetor); 
 		
-		vetor.remove(1);
+		int pos = vetor.busca(c4);
+		if (pos > -1) {
+			System.out.println(c4 + " | Elemento existe no vetor.");
+		} else {
+			System.out.println("Elemento não existe no vetor.");
+		}
 		
-		System.out.println(vetor); //exibe a terceira posição da busca
-		
-		vetor.adiciona(0, "A");
-		vetor.adiciona(2, "C");
-		System.out.println(vetor);
-
 	}
 
 }
