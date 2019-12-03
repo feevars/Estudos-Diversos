dd3494f2ba91f0bc46f56f16ef595ef27842c1a6
Fernanda Reis <fpreis98@gmail.com>
2019-12-03 02:10:44 -0300

    spring project running

---
+++ Spring Boot/cursomc/src/test/java/com/fernandareis/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandareis.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
