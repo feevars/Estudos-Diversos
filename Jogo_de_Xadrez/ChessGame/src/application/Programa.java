3cd47845549e7cdcdec2ebbc0a1f67352b1ed273
Fernanda Reis <fpreis98@gmail.com>
2019-12-08 11:39:03 -0300

    inicio do projeto de tabuleiro de xadrez

---
+++ Jogo_de_Xadrez/ChessGame/src/application/Programa.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Posicao;
+
+public class Programa {
+	public static void main(String[] args) {
+		Posicao pos = new Posicao(3, 2);
+		System.out.println(pos);
+	}
+}
