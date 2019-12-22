c19139fbdcd62542cc8a272372ace05bdaf6f49b
Fernanda Reis <fpreis98@gmail.com>
2019-12-22 01:07:51 -0300

    deposit

--- JavaFX/JavaFXProject/src/gui/ViewController.java
+++ JavaFX/JavaFXProject/src/gui/ViewController.java
@@ -1,14 +1,39 @@
 package gui;
 
+import java.util.Locale;
+
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ViewController {
-	
+
+	@FXML
+	private TextField txtDeposito;
+
+	@FXML
+	private Label labelSaldo;
+
+	@FXML
+	private Button btDeposito;
+
 	@FXML
 	private Button btTest;
-	
-	public void onBtTestAction() {
-		System.out.println("Click");
+
+	private double saldo;
+
+	public void onBtSumAction() {
+		try {
+			Locale.setDefault(Locale.US);
+			double deposito = Double.parseDouble(txtDeposito.getText());
+
+			saldo += deposito;
+			labelSaldo.setText(String.format("%.2f", saldo));
+		} catch (NumberFormatException e) {
+			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
