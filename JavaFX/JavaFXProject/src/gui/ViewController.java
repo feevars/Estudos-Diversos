e9fd76d437fbba3b5d79bc0e14d267db9548ed11
Fernanda Reis <fpreis98@gmail.com>
2019-12-22 01:36:13 -0300

    inserindo tipo de conta - ainda com erro

--- JavaFX/JavaFXProject/src/gui/ViewController.java
+++ JavaFX/JavaFXProject/src/gui/ViewController.java
@@ -1,29 +1,43 @@
 package gui;
 
+import java.net.URL;
+import java.util.List;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import gui.util.Alerts;
+import gui.util.Constraints;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Account;
 
-public class ViewController {
+public class ViewController implements Initializable{
 
 	@FXML
 	private TextField txtDeposito;
 
 	@FXML
-	private Label labelSaldo;
+	private Label labelSaldoPoupanca;
 
+	@FXML
+	private Label labelSaldoDeposito;
+	
 	@FXML
 	private Button btDeposito;
-
+	
 	@FXML
-	private Button btTest;
+	private ComboBox<Account> comboBoxAccount;
+	
+	private ObservableList<Account> obsList;
 
-	private double saldo;
+	private double saldoContaCorrente;
+	private double saldoPoupanca;
 
 	public void onBtSumAction() {
 		try {
@@ -33,7 +47,17 @@ public class ViewController {
 			saldo += deposito;
 			labelSaldo.setText(String.format("%.2f", saldo));
 		} catch (NumberFormatException e) {
-			Alerts.showAlert("Error", "Parse error", e.getMessage(), AlertType.ERROR);
+			Alerts.showAlert("Error", "Parse error", e.getMessage(), 
+					AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.setTextFieldDouble(txtDeposito);
+		Constraints.setTextFieldMaxLength(txtDeposito, 10);
+		
+		List<Account> list = new Arraylist<>();
+		
 	}
 }
