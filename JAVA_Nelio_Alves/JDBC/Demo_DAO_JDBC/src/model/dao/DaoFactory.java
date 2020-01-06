86b61724e91aadd62beb81708cef5e4e44326b2b
Fernanda Reis <fpreis98@gmail.com>
2020-01-06 15:58:24 -0300

    findById implementation

--- JAVA_Nelio_Alves/JDBC/Demo_DAO_JDBC/src/model/dao/DaoFactory.java
+++ JAVA_Nelio_Alves/JDBC/Demo_DAO_JDBC/src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.implement.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 		//não expõe implementação, apenas interface
 	}
 }
