5a6c64bc25e07800b67e034c10ef9ba8fdd66895
Fernanda Reis <fpreis98@gmail.com>
2020-01-06 13:15:00 -0300

    delete com prepareStatement

--- JavaFX/JDBC4/src/application/Program.java
+++ JavaFX/JDBC4/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -16,18 +17,20 @@ public class Program {
 			conn = DB.getConnection();
 			
 			st = conn.prepareStatement(
-					"UPDATE seller"
-					+ "SET BaseSalary = BaseSalary + ? "
-					+ "WHERE (DepartmentId = ?)");
+					"DELETE FROM seller "
+					+ "WHERE "
+					+ "Id = ?");
 			
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+			st.setInt(1	, 8);
+					
+				
+			
 			
 			int rowsAffected = st.executeUpdate();
 			
 			System.out.println("Done! Rows affected: " + rowsAffected);
 		} catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
