a4d2b091c850c6557bdfb6f93fdbd8d83afa1fd5
Fernanda Reis <fpreis98@gmail.com>
2020-02-10 19:31:47 -0300

    web services project created

---
+++ Spring Boot/WebServices/src/main/java/com/FernandaReis/WebServices/WebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.FernandaReis.WebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesApplication.class, args);
+	}
+
+}
---
+++ Spring Boot/WebServices/src/test/java/com/FernandaReis/WebServices/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FernandaReis.WebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
