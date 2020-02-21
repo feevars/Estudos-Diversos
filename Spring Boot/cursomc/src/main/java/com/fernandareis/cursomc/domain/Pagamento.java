e57237dba09749992d5435f8b742d4f0d3151b1b
Fernanda Reis <fpreis98@gmail.com>
2020-02-20 22:17:25 -0300

    JsonIgnore instead of JsonBackReference

--- Spring Boot/cursomc/src/main/java/com/fernandareis/cursomc/domain/Pagamento.java
+++ Spring Boot/cursomc/src/main/java/com/fernandareis/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fernandareis.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -24,7 +24,7 @@ public abstract class Pagamento implements Serializable {
 	
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id") //pagamento e pedido possuem o mesmo id
 	@MapsId
